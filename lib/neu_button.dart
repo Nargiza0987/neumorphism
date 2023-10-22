fd25a29306a7ff20fb9d6dd44df73bcc14de446a
Nargiza0987 <kulm1987@gmail.com>
2023-10-22 16:25:14 +0300

    sliyanie

---
+++ lib/neu_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class NeuButton extends StatelessWidget {
+  const NeuButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 180,
+      width: 180,
+      decoration: BoxDecoration(
+        color: Colors.deepPurple[300],
+        shape: BoxShape.circle,
+        boxShadow: [
+          // darker shadow at bottom right
+          BoxShadow(
+            color: Colors.deepPurple.shade700,
+            offset: Offset(4, 4),
+            blurRadius: 15,
+            spreadRadius: 1,
+          ),
+          // lighter shadow at top left
+          BoxShadow(
+            color: Colors.deepPurple.shade200,
+            offset: Offset(-4, -4),
+            blurRadius: 15,
+            spreadRadius: 1,
+          ),
+        ],
+      ),
+    );
+  }
+}
