7f1f40f16ad36d21b28449af5395fdf8a5aa4823
Nargiza0987 <kulm1987@gmail.com>
2023-10-22 16:39:14 +0300

    added neumorphism

--- lib/neu_button.dart
+++ lib/neu_button.dart
@@ -9,25 +9,36 @@ class NeuButton extends StatelessWidget {
       height: 180,
       width: 180,
       decoration: BoxDecoration(
-        color: Colors.deepPurple[300],
-        shape: BoxShape.circle,
-        boxShadow: [
-          // darker shadow at bottom right
-          BoxShadow(
-            color: Colors.deepPurple.shade700,
-            offset: Offset(4, 4),
-            blurRadius: 15,
-            spreadRadius: 1,
-          ),
-          // lighter shadow at top left
-          BoxShadow(
-            color: Colors.deepPurple.shade200,
-            offset: Offset(-4, -4),
-            blurRadius: 15,
-            spreadRadius: 1,
-          ),
-        ],
-      ),
+          color: Colors.deepPurple[300],
+          shape: BoxShape.circle,
+          boxShadow: [
+            // darker shadow at bottom right
+            BoxShadow(
+              color: Colors.deepPurple.shade700,
+              offset: Offset(4, 4),
+              blurRadius: 15,
+              spreadRadius: 1,
+            ),
+            // lighter shadow at top left
+            BoxShadow(
+              color: Colors.deepPurple.shade200,
+              offset: Offset(-4, -4),
+              blurRadius: 15,
+              spreadRadius: 1,
+            ),
+          ],
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.deepPurple.shade200,
+              Colors.deepPurple.shade400,
+            ],
+            stops: const [
+              0.1,
+              0.9,
+            ],
+          )),
     );
   }
 }
